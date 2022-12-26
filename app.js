7925935f6dbd571594ed99ef944b017c2737a0c7
Zachary Flegle <zflegle3@gmail.com>
2022-12-26 15:54:14 -0500

    Added bcrypt

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const express = require("express");
 const path = require("path");
 const session = require("express-session");
 const passport = require("passport");
+const bcrypt = require("bcrypt")
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const mongoDb = "mongodb+srv://user41:password123@cluster0.zljozht.mongodb.net/?retryWrites=true&w=majority";
+const mongoDb = "mongodb+srv://user41:zOJkqMI40CJFcWtf@cluster0.zljozht.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
@@ -21,6 +22,42 @@ const User = mongoose.model(
   })
 );
 
+// Passport Authentication Functions
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) { 
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          return done(null, user)
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: "Incorrect password" })
+        }
+      })) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
 const app = express();
 app.set("views", __dirname);
 app.set("view engine", "ejs");
@@ -29,7 +66,57 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
 
-app.get("/", (req, res) => res.render("index"));
+// Routes
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user });
+});
+
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+app.post("/sign-up", (req, res, next) => {
+  bcrypt.hash(req.body.password, 10, function (err, hashedPassword) {
+    if (err) { 
+      return next(err);
+    }
+
+    const user = new User({
+      username: req.body.username,
+      password: hashedPassword
+    }).save(err => {
+      if (err) { 
+        return next(err);
+      }
+      res.redirect("/");
+    });
+
+    // next()
+  })
+
+
+});
+
+app.get("/log-in", (req, res) => res.render("log-in-form"));
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
+  })
+);
+
+app.get("/log-out", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
